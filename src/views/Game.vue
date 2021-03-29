3660eabdb56288d874b12d85f1e4534fdd714b65
Liam S <37228196+LiamStachiw@users.noreply.github.com>
2021-03-28 20:35:24 -0400

    Added shooting functionality
    Added the ability to fire a laser in the game, restructured the code to be cleaner, added a vue.config file to allow images to be loaded properly

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,5 +1,27 @@
 <template>
-    <div ref="game" id="game" class="game">
+    <div>
+      <div ref="game" id="game" class="game">
+      </div>
+      <div class="controls">
+        <table style="width:25%;margin:auto">
+          <tr>
+            <th>Action</th>
+            <th>Control</th>
+          </tr>
+          <tr>
+            <td>Move Left</td>
+            <td>Left Arrow</td>
+          </tr>
+          <tr>
+            <td>Move Right</td>
+            <td>Right Arrow</td>
+          </tr>
+          <tr>
+            <td>Fire</td>
+            <td>Spacebar</td>
+          </tr>
+        </table>
+      </div>
     </div>
 </template>
 
@@ -9,63 +31,129 @@ import Phaser from 'phaser'
 import playerImg from '../assets/sprites/player.png'
 import playerLeftImg from '../assets/sprites/playerLeft.png'
 import playerRightImg from '../assets/sprites/playerRight.png'
+import playerLaserImg from '../assets/sprites/laserGreen.png'
 
-// create vars to be used in game creation
-let player
-let leftKey
-let rightKey
+// creating a class for the laser to use
+class Laser extends Phaser.Physics.Arcade.Sprite {
+  constructor (scene, x, y) {
+    super(scene, x, y, 'playerLaser')
+  }
 
-// execute these commands before the game is loaded
-function preload () {
-  // set the background colour
-  this.cameras.main.setBackgroundColor('#24252A')
+  fire (x, y) {
+    this.body.reset(x, y)
 
-  // load the image assets
-  this.load.image('player', playerImg)
-  this.load.image('playerLeft', playerLeftImg)
-  this.load.image('playerRight', playerRightImg)
+    this.setActive(true)
+    this.setVisible(true)
+
+    this.setVelocityY(-900)
+  }
+
+  preUpdate (time, delta) {
+    super.preUpdate(time, delta)
+
+    if (this.y <= 0) {
+      this.setActive(false)
+      this.setVisible(false)
+    }
+  }
 }
 
-// create the player
-function makePlayer (x, y) {
-  // add the image sprite to the player object
-  const player = this.add.image(x, y, 'player').setOrigin(0.5, 1)
+// creating a class for the group of player lasers to use
+class PlayerLaserGroup extends Phaser.Physics.Arcade.Group {
+  constructor (scene) {
+    super(scene.physics.world, scene)
 
-  // create a list of properties for the player
-  player.properties = {}
-  player.properties.speed = 2.5
+    this.createMultiple({
+      classType: Laser,
+      frameQuantity: 30,
+      active: false,
+      visible: false,
+      key: 'playerLaser'
+    })
+  }
 
-  // return the player object
-  return player
+  // called when a laser is to be fired
+  fireLaser (x, y) {
+    const laser = this.getFirstDead(false)
+    if (laser) {
+      laser.fire(x, y)
+    }
+  }
 }
 
-// execute these commands when creating the game
-function create () {
-  // make the player object
-  player = this.makePlayer(this.sys.canvas.width / 2, this.sys.canvas.height - 5)
+// creating a GameScene class to be used as the game itself
+class GameScene extends Phaser.Scene {
+  // used when a laser is to be fired
+  shootLaser () {
+    this.playerLaserGroup.fireLaser(this.player.x, this.player.y - 20)
+  }
 
-  // set the player scale to an appropriate size
-  player.scale = 0.6
+  // called before the game is loaded
+  preload () {
+    // load the image assets
+    this.load.image('player', playerImg)
+    this.load.image('playerLeft', playerLeftImg)
+    this.load.image('playerRight', playerRightImg)
+    this.load.image('playerLaser', playerLaserImg)
+  }
 
-  // set the keys the appropriate keycodes
-  leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
-  rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
-}
+  // used to make the playable character
+  makePlayer (x, y) {
+    // add the image sprite to the player object
+    this.player = this.add.image(x, y, 'player').setOrigin(0.5, 1)
 
-// update is executed once every frame
-function update () {
-  // if the user is pressing the 'right' key and the player is within the screen
-  if (rightKey.isDown && player.x < this.sys.canvas.width - player.displayWidth * player.originX) {
-    // move the player to the right and change the sprite
-    player.x += player.properties.speed
-    player.setTexture('playerRight')
-  // if the user is pressing the 'left' key and the player is within the screen
-  } else if (leftKey.isDown && player.x > 0 + player.displayWidth * player.originX) {
-    // move the player to the left and change the sprite
-    player.x -= player.properties.speed
-    player.setTexture('playerLeft')
-  } else {
-    player.setTexture('player')
+    // create a list of properties for the player
+    this.player.properties = {}
+    this.player.properties.speed = 2.5
+    this.player.properties.laserSpeed = 5
+    this.player.properties.maxLasers = 1
+  }
+
+  // used to create the game itself
+  create () {
+    // set the background colour
+    this.cameras.main.setBackgroundColor('#24252A')
+
+    // make a laser group
+    this.playerLaserGroup = new PlayerLaserGroup(this)
+
+    // make the player object
+    this.makePlayer(this.sys.canvas.width / 2, this.sys.canvas.height - 5)
+
+    // set the player scale to an appropriate size
+    this.player.scale = 0.6
+
+    // set the keys the appropriate keycodes
+    this.leftKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
+    this.rightKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT)
+    this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)
+  }
+
+  // update() is called once every frame
+  update () {
+    /*
+    *     Player Movement
+    */
+    // if the user is pressing the 'right' key and the player is within the screen
+    if (this.rightKey.isDown && this.player.x < this.sys.canvas.width - this.player.displayWidth * this.player.originX) {
+      // move the player to the right and change the sprite
+      this.player.x += this.player.properties.speed
+      this.player.setTexture('playerRight')
+    // if the user is pressing the 'left' key and the player is within the screen
+    } else if (this.leftKey.isDown && this.player.x > 0 + this.player.displayWidth * this.player.originX) {
+      // move the player to the left and change the sprite
+      this.player.x -= this.player.properties.speed
+      this.player.setTexture('playerLeft')
+    } else {
+      this.player.setTexture('player')
+    }
+
+    /*
+    *   Player Firing
+    */
+    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+      this.shootLaser()
+    }
   }
 }
 
@@ -83,15 +171,14 @@ export default {
       type: Phaser.AUTO,
       width: 800,
       height: 600,
-      scene: {
-        preload: preload,
-        create: create,
-        update: update,
-
-        extend: {
-          makePlayer: makePlayer
+      physics: {
+        default: 'arcade',
+        arcade: {
+          debug: false,
+          gravity: { y: 0 }
         }
-      }
+      },
+      scene: GameScene
     }
     // create the game
     this.game = new Phaser.Game(config)
