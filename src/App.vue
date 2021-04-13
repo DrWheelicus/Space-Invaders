6840c40246966ac5887aad4fdbf02d3a2e3f0c9c
DrWheelicus <hayden.macintyre@ontariotechu.net>
2021-04-13 19:11:17 -0400

    Finished front end... for now

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,15 @@
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 @import url('https://fonts.googleapis.com/css2?family=Space+Mono&display=swap');
+@import '../node_modules/@syncfusion/ej2-base/styles/material.css';
+@import '../node_modules/@syncfusion/ej2-buttons/styles/material.css';
 
 #app {
   font-family: 'Space Mono', monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  color: white;
 }
 </style>
 
