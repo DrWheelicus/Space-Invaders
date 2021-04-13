dc5f35389346003b1fd4d0350308976a41dee216
DrWheelicus <hayden.macintyre@ontariotechu.net>
2021-04-12 21:57:45 -0400

    Started front end overhaul

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Game Logo" src="../assets/logo.png" ref="/src/views/Home.vue">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import PlayOptions from '@/components/PlayOptions.vue' // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    PlayOptions
   }
 })
 export default class Home extends Vue {}
