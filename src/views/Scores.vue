da736754934c8d12ae4c5b2e9b7092e0fd3a6df4
CheyenneC-ontechu <cheyenne.carrier@ontariotechu.net>
2021-04-17 20:02:36 -0400

    basic display

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,5 +1,19 @@
 <template>
     <div class="scores">
-        <h1>This is an about page</h1>
+        <ScoreTable :scores="scores" />
     </div>
 </template>
+
+<script>
+import ScoreTable from '@/components/ScoreTable.vue'
+
+export default {
+  name: 'scoretable',
+  components: { ScoreTable },
+  data () {
+    return {
+      scores: [{ id: 0, name: 'chey', score: 70 }, { id: 1, name: 'me', score: 700 }]
+    }
+  }
+}
+</script>
