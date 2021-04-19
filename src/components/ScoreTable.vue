dffb9d53c168a42f9e5663346b3464afac6ac43e
CheyenneC-ontechu <cheyenne.carrier@ontariotechu.net>
2021-04-19 12:41:54 -0400

    styled table and added little animations

--- src/components/ScoreTable.vue
+++ src/components/ScoreTable.vue
@@ -1,7 +1,9 @@
 <template>
 <div class="scoretable">
         <h1>High Scores</h1>
-
+        <img id="player" v-on:click="playanimation()" src="../assets/sprites/player.png">
+        <img id="player2" v-on:click="playanimation2()" src="../assets/sprites/player.png">
+        <img id="player3" v-on:click="playanimation3()" src="../assets/sprites/player.png">
         <table>
             <tr v-bind:key="score.id" v-for="score in scores">
                 <td>{{score.name}}</td>
@@ -12,10 +14,57 @@
 </template>
 
 <script>
+import $ from 'jquery'
+
 export default {
   name: 'ScoreTable',
   props: {
     scores: Array
+  },
+  methods: {
+    playanimation () {
+      let id = null
+      const ship = $('#player')
+      let size = 50
+      clearInterval(id)
+      id = setInterval(function () {
+        if (size === 350) {
+          clearInterval(id)
+        } else {
+          size--
+          ship.css('height', size + 'px')
+        }
+      }, 5)
+    },
+    playanimation2 () {
+      let id = null
+      const ship = $('#player2')
+      let size = 50
+      clearInterval(id)
+      id = setInterval(function () {
+        if (size === 350) {
+          clearInterval(id)
+        } else {
+          size--
+          ship.css('height', size + 'px')
+        }
+      }, 5)
+    },
+    playanimation3 () {
+      let id = null
+      const ship = $('#player3')
+      let size = 50
+      clearInterval(id)
+      id = setInterval(function () {
+        if (size === 350) {
+          clearInterval(id)
+        } else {
+          size--
+          ship.css('height', size + 'px')
+        }
+      }, 5)
+    }
+
   }
 }
 </script>
@@ -23,11 +72,42 @@ export default {
 <style lang="scss">
 h1{
     font-family: 'Press Start 2P', cursive;
+    font-size:250%
 }
 table{
     margin-left: auto;
     margin-right: auto;
     color: white;
     font-family: 'Press Start 2P', cursive;
+    padding-top:2rem;
+}
+
+tr:first-child{
+    background: -webkit-linear-gradient(#1617e9, #0ac4f5);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+tr:nth-child(2){
+    background: -webkit-linear-gradient(#f20d0d, #f88907);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+tr:nth-child(3){
+    background: -webkit-linear-gradient(#cbff00, #1ce3b1);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+
+td {
+    font-size: 200%;
+    padding-left: 50px;
+    padding-right: 50px;
+    padding-top:1rem;
+}
+
+img{
+    margin-right: auto;
+    margin-left:auto;
+    padding: 3px;
 }
 </style>
