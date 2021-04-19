6330ab60b4a6b69f63c17636d12e6823869d9eed
DrWheelicus <hayden.macintyre@ontariotechu.net>
2021-04-19 16:36:22 -0400

    Formatted for ESLint

--- onlinechat.js
+++ onlinechat.js
@@ -1,39 +1,39 @@
-//By: Sonika 
+/* eslint-disable no-undef */
+// By: Sonika
 
-//Retrieving the username  
-var userName = cookie.get('userName');
+// Retrieving the username
+let userName = cookie.get('userName')
 if (!userName) {
-
-  userName = prompt('Choose your username:');
+  userName = prompt('Choose your username:')
   if (!userName) {
-    alert('Invalid username!');
+    alert('Invalid username!')
   } else {
-    cookie.set('user', userName);
+    cookie.set('user', userName)
   }
 }
 
-var socket = io();
+const socket = io()
 
-socket.on('count', function(data) {
-  $('.userCount').html(data);
-});
+socket.on('count', function (data) {
+  $('.userCount').html(data)
+})
 
-socket.on('message', function(data) {
-  $('.chat').append('<p><strong>' + data.userName + '</strong>: ' + data.userMessage + '</p>');
-});
+socket.on('message', function (data) {
+  $('.chat').append('<p><strong>' + data.userName + '</strong>: ' + data.userMessage + '</p>')
+})
 
 $('form').submit(function (i) {
-  i.preventDefault();
+  i.preventDefault()
 
   // Retrieve the message from the user
-  var userMessage = $(i.target).find('input').val();
+  const userMessage = $(i.target).find('input').val()
 
-    //Below we are sending the message
+  // Below we are sending the message
   socket.emit('message', {
     userName: cookie.get('user') || 'Anonymous',
     userMessage: userMessage
-  });
+  })
 
-  i.target.reset();
-  $(i.target).find('input').focus();
-});
+  i.target.reset()
+  $(i.target).find('input').focus()
+})
